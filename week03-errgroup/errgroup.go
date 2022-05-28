1fd7a673457fed6787a1e08d962c8125320cde6a
tangyumeng <tangyumeng@qiyi.com>
2022-05-28 22:13:20 +0800

    week 3 作业

--- week03-errgroup/errgroup.go
+++ week03-errgroup/errgroup.go
@@ -3,77 +3,65 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
-	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
-type MyHandler struct {
+//启动 HTTP server
+func StartHttpServer(srv *http.Server) error {
+	http.HandleFunc("/hello", HelloServer2)
+	fmt.Println("http server start")
+	err := srv.ListenAndServe()
+	return err
 }
 
-func (h MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("this is home page"))
+// 增加一个 HTTP hanlder
+func HelloServer2(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
 }
 
-// 1. 基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
+//1. 基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
 func main() {
+	ctx := context.Background()
+	// 定义 withCancel -> cancel() 方法 去取消下游的 Context
+	ctx, cancel := context.WithCancel(ctx)
+	// 使用 errgroup 进行 goroutine 取消
+	group, errCtx := errgroup.WithContext(ctx)
+	//http server
+	srv := &http.Server{Addr: ":9090"}
 
-	g, ctx := errgroup.WithContext(context.Background())
-	// https://golang.google.cn/doc/go1.17#vet
-	// 注意自1.17开始这里的chan必须是带缓冲的
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	group.Go(func() error {
+		return StartHttpServer(srv)
+	})
 
-	go func() {
-		<-signalChan            // 此处没有系统信号时阻塞，后续代码不执行，有信号时后续代码执行
-		signal.Stop(signalChan) // 显式停止监听系统信号
-		close(signalChan)       // 显式关闭监听信号的通道
-	}()
+	group.Go(func() error {
+		<-errCtx.Done() //阻塞
+		fmt.Println("http server stop")
+		return srv.Shutdown(errCtx) // 关闭httpserver
+	})
 
-	mh := MyHandler{}
-	http.Handle("/", mh)
+	chanel := make(chan os.Signal, 1)
+	signal.Notify(chanel)
 
-	srv := &http.Server{
-		Addr: ":8081",
+	group.Go(func() error {
+		for {
+			select {
+			case <-errCtx.Done(): //cancel、timeout、deadline 都可能导致 Done 被 close
+				return errCtx.Err()
+			case <-chanel: // 因为 kill -9 或其他而终止
+				cancel()
+			}
+		}
+		return nil
+	})
+
+	if err := group.Wait(); err != nil {
+		fmt.Println("group error: ", err)
 	}
+	fmt.Println("all group done!")
 
-	// 主进程（主协程）阻塞channel，以便控制http-server退出后才退出主进程（主协程）
-	// 就是一个简单的空结构体channel
-	idleCloser := make(chan struct{})
-
-	g.Go(func() error {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			close(idleCloser)
-			return err
-		}
-		return nil
-	})
-
-	g.Go(func() error {
-		// 启动一个监听系统信号控制的channel
-		<-signalChan
-
-		// 超时context
-		_, timeoutCancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer timeoutCancel()
-
-		if err := srv.Shutdown(ctx); err != nil {
-			fmt.Println("\nHttp服务暴力停止，一般是达到超时context的时间当前还有尚未完结的http请求：" + err.Error())
-		} else {
-			fmt.Println("\nHttp服务优雅停止")
-		}
-
-		// 关闭 主进程（主协程）阻塞channel
-		close(idleCloser)
-
-		return nil
-	})
-
-	g.Wait()
-
-	fmt.Println("exit successful")
 }
